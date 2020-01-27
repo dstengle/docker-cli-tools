a6e003bd4ef05e8ef52353fbaee8f21f14fd2698
David Stenglein <dave@davidstenglein.com>
2020-01-27 08:35:23 -0500

    First commit of wrapper for code-server. Not currently able to open individual files.

---
+++ scripts/code-server.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#
+# script to run code-server in docker on a specific path
+# and spawn a browser tab to the UI
+# 
+# currently, passing specific file not working
+
+
+ARG_PATH=$1
+TARGET_FILE=
+ABS_DIRECTORY=
+
+if [ -d $ARG_PATH ]
+then
+ABS_DIRECTORY=$(cd $ARG_PATH; pwd) 
+else
+DIRNAME=$(dirname $ARG_PATH)
+ABS_DIRECTORY=$(cd $DIRNAME; pwd)
+TARGET_FILE=$(basename $ARG_PATH)
+fi
+
+
+BASE_DIRNAME=$(basename $ABS_DIRECTORY)
+CONTAINER_DIR="/home/coder/$BASE_DIRNAME"
+CONTAINER_OPEN_ARG="$CONTAINER_DIR/$TARGET_FILE"
+#CONTAINER_OPEN_ARG="$CONTAINER_DIR"
+
+set -x
+
+DOCKER_NAME="code-server"
+
+docker run --rm --name "$DOCKER_NAME" -p 127.0.0.1:8080:8080  \
+  -v "$ABS_DIRECTORY:$CONTAINER_DIR" codercom/code-server:v2 \
+  --auth none $CONTAINER_DIR $CONTAINER_OPEN_ARG & 
+
+
+trap "docker stop $DOCKER_NAME" EXIT
+
+sleep 1
+
+xdg-open "http://127.0.0.1:8080"
+
+
+read -rs -n1
